1d7333fb4e0a55b2a613f95923a65b54fd704900
zetrey <sergekota@gmail.com>
2025-10-12 18:25:17 +0000

    fix: Remove required attribute from celluleId in EditUserModal

--- src/backoffice/components/EditUserModal.tsx
+++ src/backoffice/components/EditUserModal.tsx
@@ -224,7 +224,7 @@ const EditUserModal: React.FC<EditUserModalProps> = ({
                                 name="celluleId"
                                 value={formData.celluleId}
                                 onChange={handleChange}
-                                required
+                                
                                 className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 bg-white"
                             >
                                 <option value="">Choisir une cellule...</option>
